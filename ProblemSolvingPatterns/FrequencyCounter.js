16fc6a0fd9bf01596bf30e143d9f458376397bda
iklimduman <iklimdumann@gmail.com>
2022-05-03 22:35:08 +0300

    add frequency counter

---
+++ ProblemSolvingPatterns/FrequencyCounter.js
@@ -0,0 +1,22 @@
+// write a function called same that accepts 2 input arrays,
+// return true if the second array contains corresponding squared values of first array
+// frequncy matter
+// same([1,2,3],[1,4,9])  ; true
+// same([1,2,1],[1,4]) ; false bc of frequency
+// same([1,2,3],[1,4]) ; false
+
+function same(inputArr,squareArr){
+    var inputObj = {} ;
+    var squareObj = {} ;
+    inputArr.forEach(element => {
+        inputObj[element] = ++inputObj[element] || 1
+    });
+    squareArr.forEach(element => {
+        squareObj[element] = ++squareObj[element] ||  1
+    }) ;
+
+    console.log(inputObj) ;
+    console.log(squareObj) ;
+}
+
+same([1,1,1,2,4,5,6],[1,2,3,3,4,5]) ;
